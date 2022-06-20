a92888922d682d51a0528866c7a14b38c3670a5a
ludobegins <ludo.beghin@gmail.com>
2022-06-20 15:56:58 -0300

    init draw

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import * as mapboxgl from 'mapbox-gl';
+import * as Mapboxgl from 'mapbox-gl';
+import * as MapboxDraw from '@mapbox/mapbox-gl-draw';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,11 @@ import * as mapboxgl from 'mapbox-gl';
 })
 export class AppComponent implements OnInit {
   title = 'angular-mapbox';
-  map!: mapboxgl.Map;
+  map!: Mapboxgl.Map;
+  draw!: MapboxDraw;
 
   ngOnInit() {
-    this.map = new mapboxgl.Map({
+    this.map = new Mapboxgl.Map({
       accessToken: environment.mapboxKey,
       container: 'mapa-mapbox',
       style: 'mapbox://styles/mapbox/streets-v11',
@@ -21,7 +23,20 @@ export class AppComponent implements OnInit {
     });
     
     // Add zoom and rotation controls to the map.
-    this.map.addControl(new mapboxgl.NavigationControl());
+    this.map.addControl(new Mapboxgl.NavigationControl());
+
+    this.draw = new MapboxDraw({
+      displayControlsDefault: false,
+      // Select which mapbox-gl-draw control buttons to add to the map.
+      controls: {
+      polygon: true,
+      trash: true
+      },
+      // Set mapbox-gl-draw to draw by default.
+      // The user does not have to click the polygon control button first.
+      defaultMode: 'draw_polygon'
+      });
+      this.map.addControl(this.draw);
   }
 
 }
