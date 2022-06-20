c416a591366cd49c2dd6ecb88626fd12acf10aa6
ludobegins <ludo.beghin@gmail.com>
2022-06-20 11:47:00 -0300

    map routing

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MapComponent } from './map/map.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MapComponent},
+  {path: 'home', component: MapComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
